c4c4a0f3d7a7a272fe2e1463d4834beed4119289
ji-yeon-k <zxyeonx@gmail.com>
2023-07-13 22:46:54 +0900

    make_modal

---
+++ (1).modeal/js/script.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById('popupBtn');
+const modal = document.getElementById('modalWrap');
+const closeBtn = document.getElementById('closeBtn');
+
+btn.onclick = function() {
+  modal.style.display = 'block';
+}
+
+closeBtn.onclick = function() {
+  modal.style.display = 'none';
+}
+
+window.onload = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
